64d39db0a8721e88713a951911cb578f23ab78db
Leslie Strauss <leslie.strauss@gmail.com>
2015-03-09 23:56:57 -0700

    Set up routes in api.js.

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var things = require('./api/thing');
+
+// Things routes
+router.route('/things')
+  .thing(function(req, res) {
+    things.addThing(req, res)
+  })
+  .get(function(req, res) {
+    things.getAllThings(req, res)
+  });
+
+// Single thing routes
+router.route('/things/:thing_id')
+  .get(function(req, res) {
+    things.getSingleThing(req, res, req.params.thing_id)
+  })
+  .put(function(req, res) {
+    things.updateThing(req, res, req.params.thing_id)
+  })
+  .delete(function(req, res) {
+    things.deleteThing(req, res, req.params.thing_id)
+  });
+
+module.exports = router;
